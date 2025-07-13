dbf2a0d5c8e73fb65e42db2723ba8592ba33a2a0
Breno <breno@brenoferreira.com.br>
2025-07-13 14:26:01 -0300

    Feat: Added TS support - Installed TS modules; - Added support to create components using TS.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,9 +6,9 @@
 
         <title>{{ config('app.name') }}</title>
         @viteReactRefresh
-        @vite('resources/js/app.jsx')
+        @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.jsx",
+    "./resources/**/*.ts",
+    "./resources/**/*.tsx",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+      colors: {
+        primary: {
+          50: '#eff6ff',
+          100: '#dbeafe',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
